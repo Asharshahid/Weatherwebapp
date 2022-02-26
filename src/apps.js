c94a8b563e347d865cce514e4780351ff599de42
Ashar <74727596+Asharshahid@users.noreply.github.com>
2022-02-26 23:45:36 +0500

    adding start script for heroku

--- src/apps.js
+++ src/apps.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const path =require('path');
+const path = require('path');
 const hbs = require('hbs');
 const app = express();
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 // const port =3000;
 
 // public static path
-const static_path = path.join( __dirname , "../public");
-const template_path = path.join(__dirname,"../templates/views")
-const partials_path = path.join(__dirname,"../templates/partials")
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views")
+const partials_path = path.join(__dirname, "../templates/partials")
 
 
-app.set('view engine' , 'hbs');
+app.set('view engine', 'hbs');
 app.set("views", template_path)
 hbs.registerPartials(partials_path)
 
@@ -19,20 +19,20 @@ app.use(express.static(static_path));
 
 
 // ROUTING 
-app.get("", (req , res)=>{
+app.get("", (req, res) => {
     res.render('index');
 })
-app.get("/about", (req , res)=>{
+app.get("/about", (req, res) => {
     res.render('about');
 })
-app.get("/weather",(req , res)=>{
+app.get("/weather", (req, res) => {
     res.render('weather');
 })
-app.get("*",(req , res)=>{
+app.get("*", (req, res) => {
     res.render('error404');
 })
 
 
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log(`Run at port number ${port}`)
 });
